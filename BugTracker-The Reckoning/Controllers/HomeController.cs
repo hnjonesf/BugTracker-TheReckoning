6ddafc17dc4b8874fc37543a7dfbad7203207550
Hugh Jones <hughjones@libreworx.com>
2015-02-09 17:30:08 -0500

    SendGrid

--- BugTracker-The Reckoning/Controllers/HomeController.cs
+++ BugTracker-The Reckoning/Controllers/HomeController.cs
@@ -3,7 +3,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using SendGrid;
 using System.Web.Mvc;
+using System.Configuration;
+using System.Net.Mail;
+using System.Net;
 
 namespace BugTracker_The_Reckoning.Controllers
 {
@@ -24,7 +28,28 @@ namespace BugTracker_The_Reckoning.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(Contact ContactForm)
+        {
+            var MyAddress = ConfigurationManager.AppSettings["ContactEmail"];
+            var MyUsername = ConfigurationManager.AppSettings["Username"];
+            var MyPassword = ConfigurationManager.AppSettings["Password"];
+
+            if (ModelState.IsValid)
+            {
+                SendGridMessage mail = new SendGridMessage();
+                mail.From = new MailAddress(ContactForm.Email);
+                mail.AddTo(MyAddress);
+                mail.Subject = ContactForm.Subject;
+                mail.Text = ContactForm.Message;
+                var credentials = new NetworkCredential(MyUsername, MyPassword);
+                var transportWeb = new Web(credentials);
+                transportWeb.Deliver(mail);
+            }
 
             return View();
         }
--- BugTracker-The Reckoning/Models/IdentityModels.cs
+++ BugTracker-The Reckoning/Models/IdentityModels.cs
@@ -6,9 +6,40 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using SendGrid;
+using System.Configuration;
+using System.Net.Mail;
+using System.Net;
 
 namespace BugTracker_The_Reckoning.Models
 {
+
+    // SendGrid 
+
+    public class EmailService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            var MyAddress = ConfigurationManager.AppSettings["ContactEmail"];
+            var MyUserName = ConfigurationManager.AppSettings["UserName"];
+            var MyPassword = ConfigurationManager.AppSettings["Password"];
+
+            SendGridMessage mail = new SendGridMessage();
+            mail.From = new MailAddress("noreply");
+            mail.AddTo(message.Destination);
+            mail.Subject = message.Subject;
+            mail.Text = message.Body;
+
+            var credentials = new NetworkCredential(MyUserName, MyPassword);
+            var transportWeb = new Web(credentials);
+            transportWeb.Deliver(mail);
+
+            return Task.FromResult(0);
+        }
+
+    }
+   
+
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
 
     public class ApplicationUser : IdentityUser
